fe3955a1897571d48c6ec7fcdd3cf4cbd27ddac4
Melissa Usher <MelEUsher17@gmail.com>
2025-07-18 18:48:43 +0200

    GREEN-002: Update function_1 to pass

--- MelUTest1.py
+++ MelUTest1.py
@@ -1,11 +1,11 @@
-# NAME:         FIXME
+# NAME:         Melissa
 
 # Example
 def hello_world():
     return "Hello!"
 
 def function_1(data):
-    return True
+    return data in [1,2]
 
 def main():
     print("Function 1: ", function_1(1))
